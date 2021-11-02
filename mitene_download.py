f5b8d4995097fe294ef973610c732d9faa80e0ff
Jérome Perrin <perrinjerome@gmail.com>
2021-11-02 09:25:42 +0900

    use yapf to format code

--- mitene_download.py
+++ mitene_download.py
@@ -36,8 +36,8 @@ def main() -> None:
     page = 0
     while True:
       r = session.get(f"{args.album_url}?page={page}")
-      page_text = r.text.split("//<![CDATA[\nwindow.gon={};gon.media=")[1].split(
-          ";gon.familyUserIdToColorMap=")[0]
+      page_text = r.text.split("//<![CDATA[\nwindow.gon={};gon.media="
+                               )[1].split(";gon.familyUserIdToColorMap=")[0]
       data = json.loads(page_text)
 
       page += 1
@@ -48,7 +48,10 @@ def main() -> None:
         expiringUrl = media.get("expiringVideoUrl", media["expiringUrl"])
         filename = urllib.parse.urlparse(expiringUrl).path.split("/")[-1]
         filename = f'{media["tookAt"]}-{filename}'
-        destination_filename = os.path.join(args.destination_directory, filename)
+        destination_filename = os.path.join(
+            args.destination_directory,
+            filename,
+        )
 
         if not os.path.exists(destination_filename):
           with open(destination_filename + ".tmp", "wb") as f:
@@ -63,10 +66,13 @@ def main() -> None:
           with open(comment_filename + ".tmp", "w") as comment_f:
             for comment in media["comments"]:
               if not comment["isDeleted"]:
-                comment_f.write(f'**{comment["user"]["nickname"]}**: {comment["body"]}\n\n')
+                comment_f.write(
+                    f'**{comment["user"]["nickname"]}**: {comment["body"]}\n\n'
+                )
           os.rename(comment_filename + ".tmp", comment_filename)
 
       print(f'processed page {page}')
+
 
 if __name__ == '__main__':
   main()
