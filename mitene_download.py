5eaefb96661e608c8e3377b80cd9ce16eb40fc94
suasive93 <20932109+suasive93@users.noreply.github.com>
2023-10-10 22:56:43 -0400

    Formatted to yapf standards

--- mitene_download.py
+++ mitene_download.py
@@ -1,6 +1,6 @@
 """
-mitene_download.py: 
-A script to download media and comments from mitene album URLs. It handles pagination, 
+mitene_download.py:
+A script to download media and comments from mitene album URLs. It handles pagination,
 concurrent downloads, and supports password-protected albums.
 """
 
@@ -13,26 +13,32 @@ import os
 import sys
 import urllib.parse
 import mimetypes
-from typing import Awaitable, Optional 
+from typing import Awaitable, Optional
 from tqdm import tqdm
 import aiohttp
 
 comments_saved_counter = 0
 
-class TqdmUpTo(tqdm): # type: ignore
+
+class TqdmUpTo(tqdm):  # type: ignore
     def update_to(self, b: int = 1, bsize: int = 1, tsize: Optional[int] = None) -> None:
         if tsize is not None:
             self.total = tsize
         self.update(b * bsize - self.n)
 
+
 async def gather_with_concurrency(n: int, *tasks: Awaitable[None]) -> None:
     semaphore = asyncio.Semaphore(n)
+
     async def sem_task(task: Awaitable[None]) -> None:
         async with semaphore:
             await task
+
     await asyncio.gather(*(sem_task(task) for task in tasks))
 
-async def download_media(session: aiohttp.ClientSession, url: str, destination_filename: str, media_name: str, verbose: bool, current_index: int, total_media: int, max_retries: int = 3) -> None:
+
+async def download_media(session: aiohttp.ClientSession, url: str, destination_filename: str, media_name: str,
+                         verbose: bool, current_index: int, total_media: int, max_retries: int = 3) -> None:
     for attempt in range(max_retries):
         try:
             if not os.path.exists(destination_filename):
@@ -40,29 +46,39 @@ async def download_media(session: aiohttp.ClientSession, url: str, destination_f
                     r.raise_for_status()
                     content_type = r.headers.get('Content-Type', '')
                     file_extension = mimetypes.guess_extension(content_type)
-                    
-                    if file_extension and not destination_filename.endswith(file_extension) and not (destination_filename.endswith('.jpg') and file_extension == '.jpeg'):
+
+                    if file_extension and not destination_filename.endswith(
+                            file_extension) and not (
+                                    destination_filename.endswith('.jpg') and file_extension == '.jpeg'):
                         destination_filename += file_extension
-                    
+
                     with open(destination_filename, "wb") as f:
                         total_size = int(r.headers.get('content-length', 0))
-                        with TqdmUpTo(unit='B', unit_scale=True, unit_divisor=1024, total=total_size,
-                                      desc=f"Downloading {current_index} of {total_media}",
-                                      leave=False, mininterval=0.1, ncols=100, 
-                                      bar_format='{desc}: [{bar}] {percentage:3.0f}% - {n_fmt}/{total_fmt}') as pbar:
+                        with TqdmUpTo(
+                                unit='B',
+                                unit_scale=True,
+                                unit_divisor=1024,
+                                total=total_size,
+                                desc=f"Downloading {current_index} of {total_media}",
+                                leave=False,
+                                mininterval=0.1,
+                                ncols=100,
+                                bar_format='{desc}: [{bar}] {percentage:3.0f}% - {n_fmt}/{total_fmt}',
+                        ) as pbar:
                             async for chunk in r.content.iter_chunked(1024):
                                 f.write(chunk)
                                 pbar.update(len(chunk))
             elif verbose:
                 print(f"{media_name} already downloaded.")
-            break  
+            break
         except aiohttp.ClientPayloadError:
             if attempt < max_retries - 1:
                 print(f"Retry {attempt + 1} for {media_name} due to ClientPayloadError")
-                continue  
+                continue
             else:
                 print(f"Failed to download {media_name} after {max_retries} attempts due to ClientPayloadError")
                 break
+
 
 async def async_main() -> None:
     global comments_saved_counter
@@ -89,13 +105,17 @@ async def async_main() -> None:
                     print('Album is password protected, please specify password with --password', file=sys.stderr)
                     sys.exit(1)
                 authenticity_token = response_text.split('name="authenticity_token" value="')[1].split('"')[0]
-                r = await session.post(f"{args.album_url}/login", data={'session[password]': args.password, 'authenticity_token': authenticity_token})
+                r = await session.post(f"{args.album_url}/login", data={
+                    'session[password]': args.password,
+                    'authenticity_token': authenticity_token
+                })
                 if r.url.path.endswith('/login'):
                     print('Could not authenticate, maybe password is incorrect', file=sys.stderr)
                     sys.exit(1)
                 continue
 
-            page_text = response_text.split("//<![CDATA[\nwindow.gon={};gon.media=")[1].split(";gon.familyUserIdToColorMap=")[0]
+            page_text = response_text.split("//<![CDATA[\nwindow.gon={};gon.media=")[1].split(
+                ";gon.familyUserIdToColorMap=")[0]
             data = json.loads(page_text)
 
             page += 1
@@ -106,18 +126,18 @@ async def async_main() -> None:
                 filename = f'{media["tookAt"].replace(":", "_")}-{filename}'
                 destination_filename = os.path.join("downloaded", filename)
 
-                
                 media_downloaded = os.path.exists(destination_filename)
-                
-                
+
                 if not media_downloaded:
-                    download_coroutines.append(download_media(session, f"{args.album_url}/media_files/{media['uuid']}/download", destination_filename, media['uuid'], args.verbose, index+1, len(data["mediaFiles"])))
-                
-             
+                    download_coroutines.append(
+                        download_media(session, f"{args.album_url}/media_files/{media['uuid']}/download",
+                                       destination_filename, media['uuid'], args.verbose, index + 1,
+                                       len(data["mediaFiles"])))
+
                 if media["comments"]:
                     comment_filename = os.path.join("comments", os.path.splitext(filename)[0] + ".md")
                     comment_filename = comment_filename.replace(':', '_')
-                    if not os.path.exists(comment_filename):  
+                    if not os.path.exists(comment_filename):
                         with open(comment_filename, "w", encoding='utf-8') as comment_f:
                             for comment in media["comments"]:
                                 if not comment["isDeleted"]:
@@ -128,9 +148,11 @@ async def async_main() -> None:
         await gather_with_concurrency(4, *download_coroutines)
         await session.close()
 
+
 def main() -> None:
     loop = asyncio.get_event_loop()
     loop.run_until_complete(async_main())
+
 
 if __name__ == '__main__':
     try:
