c3f794f3e39ac819b649f372db5c8d0fe1c420ac
Jérome Perrin <jerome@nexedi.com>
2025-06-29 21:06:49 +0900

    Update for new javascript
    Fixes #21

--- mitene_download.py
+++ mitene_download.py
@@ -111,7 +111,7 @@ async def async_main() -> None:
           sys.exit(1)
         continue
 
-      page_text = response_text.split("//<![CDATA[\nwindow.gon={};gon.media=")[1].split(
+      page_text = response_text.split(";gon.media=")[1].split(
         ";gon.familyUserIdToColorMap="
       )[0]
       data = json.loads(page_text)
